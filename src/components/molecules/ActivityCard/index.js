4475f157a52fd19f8e867c719081fdc51c397033
irfanzidniofficial <irfanzidniofficial@gmail.com>
2021-08-11 20:57:06 +0700

    Activity Update

--- src/components/molecules/ActivityCard/index.js
+++ src/components/molecules/ActivityCard/index.js
@@ -3,6 +3,7 @@ import {StyleSheet, Text, View} from 'react-native';
 import {fonts, mainColors} from '../../../utils';
 import ScrollableTabView, {
   DefaultTabBar,
+  ScrollableTabBar,
 } from 'react-native-scrollable-tab-view';
 
 const AktivityCard = () => {
@@ -10,10 +11,11 @@ const AktivityCard = () => {
     <ScrollableTabView
       style={{marginTop: 20}}
       initialPage={1}
-      renderTabBar={() => <DefaultTabBar />}>
-      <Text tabLabel="Tab #1">My</Text>
+      renderTabBar={() => <ScrollableTabBar />}>
+      <Text tabLabel="Chat">My</Text>
       <Text tabLabel="Tab #2">favorite</Text>
       <Text tabLabel="Tab #3">project</Text>
+      <Text tabLabel="Tab #4">project</Text>
       {/* <Text style={styles.title}>Hello</Text> */}
       {/* <View style={styles.container}></View> */}
     </ScrollableTabView>
